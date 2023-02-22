0152aa34fa0b3ca70b09a24e28252ce274ef7cd1
Korèn <kpapazian@edenschool.fr>
2023-02-22 16:25:53 +0100

    btn

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-let p = document.getElementsByClassName("p");
-let f = document.getElementsByClassName("f");
-let c = document.getElementsByClassName("c");
+let p = document.getElementById("p");
+let f = document.getElementById("f");
+let c = document.getElementById("c");
 
 let imgs_left = document.getElementById("imgs_left");
 let imgs_right = document.getElementById("imgs_right");
@@ -11,34 +11,52 @@ let ia_picture = document.getElementById("ia_picture");
 let pfc = ["../img/pierre.png", "../img/feuille.png", "../img/ciseaux.png"];
 let ia = Math.floor(Math.random() * (pfc.length - 0)) + 0;
 
-for (let i = 0; i < 2; i++) {
-    p[i].addEventListener("click", () => {
-        console.log("p");
-        player_picture.src = "../img/pierre.png";
-        player_picture.style.display = "block";
 
-        ia = Math.floor(Math.random() * (pfc.length - 0)) + 0;
-        ia_picture.src = pfc[ia];
-        ia_picture.style.display = "block";
-    });
+p.addEventListener("click", () => {
+    console.log("p");
+    player_picture.src = "../img/pierre.png";
+    player_picture.style.display = "block";
 
-    f[i].addEventListener("click", () => {
-        console.log("f");
-        player_picture.src = "../img/feuille.png";
-        player_picture.style.display = "block";
+    ia = Math.floor(Math.random() * (pfc.length - 0)) + 0;
+    ia_picture.src = pfc[ia];
+    ia_picture.style.display = "block";
+    winner();
+});
 
-        ia = Math.floor(Math.random() * (pfc.length - 0)) + 0;
-        ia_picture.src = pfc[ia];
-        ia_picture.style.display = "block";
-    });
+f.addEventListener("click", () => {
+    console.log("f");
+    player_picture.src = "../img/feuille.png";
+    player_picture.style.display = "block";
 
-    c[i].addEventListener("click", () => {
-        console.log("c");
-        player_picture.src = "../img/ciseaux.png";
-        player_picture.style.display = "block";
+    ia = Math.floor(Math.random() * (pfc.length - 0)) + 0;
+    ia_picture.src = pfc[ia];
+    ia_picture.style.display = "block";
+    winner();
+});
 
-        ia = Math.floor(Math.random() * (pfc.length - 0)) + 0;
-        ia_picture.src = pfc[ia];
-        ia_picture.style.display = "block";
-    });
+c.addEventListener("click", () => {
+    console.log("c");
+    player_picture.src = "../img/ciseaux.png";
+    player_picture.style.display = "block";
+
+    ia = Math.floor(Math.random() * (pfc.length - 0)) + 0;
+    ia_picture.src = pfc[ia];
+    ia_picture.style.display = "block";
+    winner();
+});
+
+function winner() {
+    setTimeout(() => {
+        if (player_picture.src == ia_picture.src) {
+            alert("execo");
+        }
+
+        if (player_picture.src = "../img/pierre.png" && ia != 1 || ia != 0) {
+            alert("Player WIN!");
+        }
+        
+        // if (player_picture.src = "../img/feuille.png" && pfc[1]) {
+        //     alert("ia win");
+        // }
+    }, 500);
 }
